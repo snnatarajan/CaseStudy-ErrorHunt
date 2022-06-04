dd6c54d84e2c64d6db86dbb9bce8b883fb76d239
snnatarajan <snn.natarajan@gmail.com>
2022-06-04 22:04:21 +0530

    updated
    updated

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('process.env.MONGODB_URI' || 'mongodb://localhost:27017/Library');
+//mongoose.connect('process.env.MONGODB_URI' || 'mongodb://localhost:27017/Library');
+mongoose.connect('mongodb+srv://atlasadmin:AXWUnxyqTN3FIrSy@ictfspproject.zml3jmi.mongodb.net/?retryWrites=true&w=majority' );
+
 const Schema = mongoose.Schema;
 
 
